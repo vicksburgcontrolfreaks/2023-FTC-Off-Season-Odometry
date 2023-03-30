cce15576e6238a3cf255cff310ac45f0b6e8249e
vicksburgcontrolfreaks <90581349+vicksburgcontrolfreaks@users.noreply.github.com>
2023-03-29 20:01:06 -0400

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -22,7 +22,7 @@ public class DriveConstants {
      * These are motor constants that should be listed online for your motors.
      */
     public static final double TICKS_PER_REV = 560;
-    public static final double MAX_RPM = 300;
+    public static final double MAX_RPM = 300; //
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
